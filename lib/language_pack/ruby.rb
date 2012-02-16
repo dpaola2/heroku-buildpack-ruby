84b8f894f0456a89481bb4f793d4fbbc5f7e240a
Dave Paola <dpaola2@gmail.com>
2012-02-16 13:00:51 -0800

    bundler config

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -55,7 +55,6 @@ class LanguagePack::Ruby < LanguagePack::Base
       create_database_yml
       install_binaries
       run_assets_precompile_rake_task
-      install_sqlite
     end
   end
 
@@ -303,6 +302,10 @@ ERROR
         # we need to set BUNDLE_CONFIG and BUNDLE_GEMFILE for
         # codon since it uses bundler.
         env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:$CPATH CPPATH=#{yaml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
+        puts run("#{env_vars} bundle config build.sqlite3-ruby --with-opt-dir=/app/lib 2>&1")
+        puts run("#{env_vars} bundle config sqlite3-ruby --with-opt-dir=/app/lib 2>&1")
+        puts run("#{env_vars} bundle config build.sqlite3 --with-opt-dir=/app/lib 2>&1")
+        puts run("#{env_vars} bundle config sqlite3 --with-opt-dir=/app/lib 2>&1")
         puts "Running: #{bundle_command}"
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
       end
