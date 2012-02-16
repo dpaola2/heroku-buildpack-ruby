081f083501002aef8e1e747c3d628ca235509df4
Dave Paola <dpaola2@gmail.com>
2012-02-16 10:41:55 -0800

    added bundle config to buildpack for sqlite3

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -295,6 +295,7 @@ ERROR
         # we need to set BUNDLE_CONFIG and BUNDLE_GEMFILE for
         # codon since it uses bundler.
         env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:$CPATH CPPATH=#{yaml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
+        puts `bundle config build.sqlite3-ruby --with-opt-dir=/app/sqlite3`
         puts "Running: #{bundle_command}"
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
 
